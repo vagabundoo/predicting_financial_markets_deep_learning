df63d77aab5fb96d9e163104265276f7da495aa8
vagabundoo <evillamorm@gmail.com>
2020-03-05 12:27:13 +0100

    trying to get timeseriesgenerator to work

--- workflow/splitSequence.py
+++ workflow/splitSequence.py
@@ -15,13 +15,3 @@ def split_sequence(sequence, n_steps):
 		X.append(seq_x)
 		y.append(seq_y)
 	return array(X), array(y)
-
-# # define input sequence
-# raw_seq = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-# # choose a number of time steps
-# n_steps = 3
-# # split into samples
-# X, y = split_sequence(raw_seq, n_steps)
-# # summarize the data
-# for i in range(len(X)):
-# 	print(X[i], y[i])
